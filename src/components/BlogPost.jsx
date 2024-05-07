03b3385b183c67d8df5da3c57aee8e9feae1a600
deoclaw <115387064+deoclaw@users.noreply.github.com>
2024-05-06 23:51:36 -0500

    load in blog posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,36 +1,59 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import Container from "./Container";
+import blogposts from "../data/blogposts.json";
 
 export default function BlogPost() {
+	const { slug } = useParams(); //need this to access the :slug in the url
+	console.log(slug);
+	console.log(blogposts);
+	const newPost = blogposts.filter((post) => post.slug === slug);
+	console.log(newPost);
+
 	return (
 		<Container>
 			<div
 				className="window"
 				style={{
 					maxWidth: "100%",
-					height: "75vh",
+					height: "100vh",
 					marginTop: "0",
 					backgroundColor: "white",
+					overflow: "hidden",
 				}}
+				key={newPost[0].slug}
 			>
 				<div className="title-bar">
-					<div className="title-bar-text">Blog</div>
+					<div className="title-bar-text">{newPost[0].title}</div>
 					<div className="title-bar-controls">
 						<button aria-label="Minimize"></button>
 						<button aria-label="Maximize"></button>
-						<NavLink to="/">
+						<NavLink to="/blog">
 							<button aria-label="Close" style={{ cursor: "pointer" }}></button>
 						</NavLink>
 					</div>
 				</div>
-				<div className="window-body">
-					<p style={{ textAlign: "center", margin: "1rem" }}>
-						Click on a blog post to read more (or click X to go back!)
-					</p>
+				<div
+					className="window-body"
+					id="blogBody"
+					style={{ maxHeight: "85vh", overflowY: "auto" }}
+				>
+					<div style={{ margin: "1rem" }} className="blogPostBody">
+						{
+							//found this on stack overflow: https://stackoverflow.com/questions/35351706/how-to-render-a-multi-line-text-string-in-react
+							newPost[0].body.split("\n").map((i, key) => {
+								return (
+									<p key={key} className="blogPara">
+										{i}
+									</p>
+								);
+							})
+						}
+					</div>
 					<div id="icon-container"></div>
 				</div>
 			</div>
+			;
 		</Container>
 	);
 }
--- src/components/BlogWindow.jsx
+++ src/components/BlogWindow.jsx
@@ -8,6 +8,7 @@ export default function BlogWindow() {
 	//i'd like to fill it with blog icons that when clicked pull the user into the specific blog post windside a similar blog window base (so blog window should be ap arent component with children?)
 
 	//i feel like i'm starting to get ah ang of react but a lot of the states i don't grok
+	const reversedBlogPosts = blogposts.reverse();
 
 	return (
 		<>
@@ -18,6 +19,7 @@ export default function BlogWindow() {
 					height: "75vh",
 					marginTop: "0",
 					backgroundColor: "white",
+					overflow: "hidden",
 				}}
 			>
 				<div className="title-bar">
@@ -30,12 +32,15 @@ export default function BlogWindow() {
 						</NavLink>
 					</div>
 				</div>
-				<div className="window-body">
+				<div
+					className="window-body"
+					style={{ maxHeight: "75vh", overflowY: "auto" }}
+				>
 					<p style={{ textAlign: "center", margin: "1rem" }}>
 						Click on a blog post to read more (or click X to go back!)
 					</p>
 					<div id="icon-container">
-						{blogposts.map((post) => (
+						{blogposts.toReversed().map((post) => (
 							<IconInWindow
 								key={post.slug}
 								id={post.slug}
